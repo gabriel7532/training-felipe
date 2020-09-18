d8d3b7f848b43e7739b8b49277e965b7501382f5
gabriel7532 <gabriel7532@hotmail.com>
2020-09-18 11:01:23 -0500

    cambios

--- src/herencia/Herencia.java
+++ src/herencia/Herencia.java
@@ -17,5 +17,11 @@ public class Herencia {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    public Canino mac = new Canino();
+    
+
+    
+    }
+.
     
 }
